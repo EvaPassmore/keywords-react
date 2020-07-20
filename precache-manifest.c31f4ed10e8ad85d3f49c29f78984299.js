e1e6b7bc7295650aafa391aed90db1a0b3fb5da8
Eva Passmore <passmore.eva@gmail.com>
2020-07-20 21:38:09 +0100

    Updates

---
+++ precache-manifest.c31f4ed10e8ad85d3f49c29f78984299.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df20b396e5f952c34610725089df464",
+    "url": "/keywords-react/index.html"
+  },
+  {
+    "revision": "10fd2d7dab603d9e434a",
+    "url": "/keywords-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a1b487ff0aca6548a7ce",
+    "url": "/keywords-react/static/css/main.9b2aecbd.chunk.css"
+  },
+  {
+    "revision": "10fd2d7dab603d9e434a",
+    "url": "/keywords-react/static/js/2.0c39b06c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/keywords-react/static/js/2.0c39b06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b487ff0aca6548a7ce",
+    "url": "/keywords-react/static/js/main.965e8fd1.chunk.js"
+  },
+  {
+    "revision": "a3beb5306241fabf60b5",
+    "url": "/keywords-react/static/js/runtime-main.c29ac2e3.js"
+  },
+  {
+    "revision": "58cb575222092ec4d8d32f8fb5a10f3d",
+    "url": "/keywords-react/static/media/sstk_viewfinder_rgb.58cb5752.png"
+  }
+]);
